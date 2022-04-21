1354fe8dd526be99758ac211282d861dff8c726b
Vipul Patel <vipuljpatel2103@gmail.com>
2022-04-21 06:25:10 -0700

    Remove garbage code.

--- src/main/java/com/doc/ocr/OcrApplication.java
+++ src/main/java/com/doc/ocr/OcrApplication.java
@@ -1,26 +1,13 @@
 package com.doc.ocr;
 
-import com.doc.ocr.processor.model.Temp;
-import com.doc.ocr.processor.model.TextSegment;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.awt.*;
 
 @SpringBootApplication
 public class OcrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcrApplication.class, args);
-		TextSegment t = TextSegment.builder().build();
-		Temp tt = Temp.builder().Rect(new Rectangle(1,2,3,4)).build();
-		try {
-			System.out.println(new ObjectMapper().writeValueAsString(tt));
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 }
