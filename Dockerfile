4771a24c8589858c316cd35b1e4bfab35b2b8a36
Vipul Patel <vipuljpatel2103@gmail.com>
2022-04-26 23:20:01 -0700

    LC_ALL=C

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 # define base docker image
-# FROM openjdk:11
-FROM mcr.microsoft.com/java/jdk:11-zulu-windowsservercore
+#FROM adoptopenjdk/openjdk11
+#FROM mcr.microsoft.com/java/jdk:11-zulu-windowsservercore
+FROM openjdk:8-jdk-alpine
+RUN apk update
+RUN apk add \
+    tesseract-ocr
 LABEL maintainer="Vipul"
 ARG JAR_FILE=build/libs/ocr-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} document-ocr.jar
---
+++ src/main/java/com/doc/ocr/processor/image/CLibrary.java
@@ -0,0 +1,16 @@
+package com.doc.ocr.processor.image;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+public interface CLibrary extends Library {
+    CLibrary INSTANCE = Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class);
+
+    int LC_CTYPE=0;
+    int LC_NUMERIC=1;
+    int LC_ALL=6;
+
+    // char *setlocale(int category, const char *locale);
+    String setlocale(int category, String locale);
+}
